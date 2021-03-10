6e92ff13279cb63e51d947f4b2fcaefc3c27b5b3
Vlad <vladyn19981998@gmail.com>
2021-03-10 04:46:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array == undefined||array.length < 1) return 0
+  let min = array[0];
+  for(const value of array) {
+      if(value < min) {
+          min = value
+      }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array == undefined||array.length < 1) return 0
+  let max = array[0];
+  for(const value of array) {
+      if(value > max) {
+          max = value;
+      }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array == undefined||array.length < 1) return 0
+  return array.reduce((acc,cur) => acc + cur,0)/array.length;
 }
